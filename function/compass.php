80fc5d8b67eb117357dda54255a40624ade5a2bb
Konata400 <34619567+Konata400@users.noreply.github.com>
2020-10-12 12:36:06 +0200

    fix update compass
    - Se añade la comprovacion de la session.

--- function/compass.php
+++ function/compass.php
@@ -18,6 +18,9 @@ Copyright (C) 2020 Cristina Ibañez, Konata400
     along with McWebPanel.  If not, see <https://www.gnu.org/licenses/>.
 */
 
+require_once("../template/session.php");
+require_once("../template/errorreport.php");
+
 function test_input($data)
 {
     $data = trim($data);
@@ -26,49 +29,59 @@ function test_input($data)
     return $data;
 }
 
+//COMPROVAR SI SESSION EXISTE SINO CREARLA CON NO
+if (!isset($_SESSION['VALIDADO']) || !isset($_SESSION['KEYSECRETA'])) {
+    $_SESSION['VALIDADO'] = "NO";
+    $_SESSION['KEYSECRETA'] = "0";
+}
+
 //VALIDAMOS SESSION
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+if ($_SESSION['VALIDADO'] == $_SESSION['KEYSECRETA']) {
 
-    if (isset($_POST['action']) && !empty($_POST['action'])) {
+    //VALIDAMOS SESSION
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-        $elerror = 0;
-        $eltexto = "<div class='alert alert-danger' role='alert'>";
-        $pwd = $_POST['action'];
+        if (isset($_POST['action']) && !empty($_POST['action'])) {
 
-        if (strlen($pwd) < 16) {
-            $eltexto .= "¡La contraseña tiene que tener un mínimo de 16 caracteres!";
-            $eltexto .= "<br>";
-            $elerror = 1;
-        }
+            $elerror = 0;
+            $eltexto = "<div class='alert alert-danger' role='alert'>";
+            $pwd = $_POST['action'];
 
-        if (!preg_match("#[0-9]+#", $pwd)) {
-            $eltexto .= "¡La contraseña tiene que contener al menos un número!";
-            $eltexto .= "<br>";
-            $elerror = 1;
-        }
-
-        if (!preg_match("#[a-z]+#", $pwd)) {
-            $eltexto .= "¡La contraseña tiene que contener al menos una letra!";
-            $eltexto .= "<br>";
-            $elerror = 1;
-        }
-
-        if (!preg_match("#[A-Z]+#", $pwd)) {
-            $eltexto .= "¡La contraseña tiene que contener al menos una letra mayúscula!";
-            $eltexto .= "<br>";
-            $elerror = 1;
-        }
-
-        if (!preg_match("#\W+#", $pwd)) {
-            if (!preg_match('#_+#', $pwd)) {
-                $eltexto .= "¡La contraseña tiene que contener al menos un símbolo!";
+            if (strlen($pwd) < 16) {
+                $eltexto .= "¡La contraseña tiene que tener un mínimo de 16 caracteres!";
                 $eltexto .= "<br>";
                 $elerror = 1;
             }
-        }
 
-        $eltexto .= "</div>";
-        $elarray = array("texto" => $eltexto, "error" => $elerror);
-        echo json_encode($elarray);
+            if (!preg_match("#[0-9]+#", $pwd)) {
+                $eltexto .= "¡La contraseña tiene que contener al menos un número!";
+                $eltexto .= "<br>";
+                $elerror = 1;
+            }
+
+            if (!preg_match("#[a-z]+#", $pwd)) {
+                $eltexto .= "¡La contraseña tiene que contener al menos una letra!";
+                $eltexto .= "<br>";
+                $elerror = 1;
+            }
+
+            if (!preg_match("#[A-Z]+#", $pwd)) {
+                $eltexto .= "¡La contraseña tiene que contener al menos una letra mayúscula!";
+                $eltexto .= "<br>";
+                $elerror = 1;
+            }
+
+            if (!preg_match("#\W+#", $pwd)) {
+                if (!preg_match('#_+#', $pwd)) {
+                    $eltexto .= "¡La contraseña tiene que contener al menos un símbolo!";
+                    $eltexto .= "<br>";
+                    $elerror = 1;
+                }
+            }
+
+            $eltexto .= "</div>";
+            $elarray = array("texto" => $eltexto, "error" => $elerror);
+            echo json_encode($elarray);
+        }
     }
 }
