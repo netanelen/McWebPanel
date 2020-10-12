e6afa2bf834975cb8970afd3a62c72ba0b1bde75
Konata400 <34619567+Konata400@users.noreply.github.com>
2020-10-13 01:36:50 +0200

    add nueva version session
    - Se comprueba la version de php
    - Se protege session del javascript
    - Se añade samesite en versiones superiores a 7.3

--- template/session.php
+++ template/session.php
@@ -16,17 +16,26 @@ Copyright (C) 2020 Cristina Ibañez, Konata400
     You should have received a copy of the GNU General Public License
     along with McWebPanel.  If not, see <https://www.gnu.org/licenses/>.
 */
-
-ini_set('session.gc_maxlifetime', 3600);
-session_set_cookie_params(3600);
-
-if ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') || $_SERVER['SERVER_PORT'] == 443) {
-
-  ini_set('session.cookie_secure', 1);
-  ini_set('session.cookie_httponly', 1);
+if (PHP_VERSION_ID < 70300) {
+  //VERSION ANTIGUA A 7.3
+  if ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') || $_SERVER['SERVER_PORT'] == 443) {
+    //SI ES HTTPS
+    session_set_cookie_params(3600, '/', $_SERVER['HTTP_HOST'], true, true);
+  } else {
+    //SI ES HTTP
+    session_set_cookie_params(3600, '/', $_SERVER['HTTP_HOST'], false, false);
+  }
+} else {
+  //version mas moderna soporte samesite
+  if ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') || $_SERVER['SERVER_PORT'] == 443) {
+    //SI ES HTTPS
+    session_set_cookie_params(3600, '/', $_SERVER['HTTP_HOST'], true, true);
+    ini_set('session.cookie_samesite', "Strict");
+  } else {
+    //SI ES HTTP
+    session_set_cookie_params(3600, '/', $_SERVER['HTTP_HOST'], false, true);
+    ini_set('session.cookie_samesite', "Strict");
+  }
 }
-ini_set('session.cookie_samesite', "Strict");
-
 session_start();
-
 ?>
